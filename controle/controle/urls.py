9703bb30e235bd24292c39348a517efd8d962f4f
pedrlucz <pedrlucz7@gmail.com>
2025-04-18 02:33:24 -0300

    comentando a inútil

--- controle/controle/urls.py
+++ controle/controle/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',  include('financas.urls')), 
-    path('adicionar/', include('financas.urls')),
+    # path('adicionar/', include('financas.urls')),
     path('accounts/', include('django.contrib.auth.urls')), # adiciona as urls de autenticação
 ]
