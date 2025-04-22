8c644cfe8755cf751ed3d5022036951a04229647
pedrlucz <pedrlucz7@gmail.com>
2025-04-21 21:54:54 -0300

    adicionando url dashboard

--- controle/financas/urls.py
+++ controle/financas/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import adicionar_transacao, lista_transacoes, home, login_register, user_logout, register_user
+from .views import adicionar_transacao, lista_transacoes, home, login_register, user_logout, register_user, dashboard
 from . import views
 
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
     path('register/', register_user, name = 'register'),
     path('login/', login_register, name = 'login'),
     path('logout/', user_logout, {'next_page':'login'}, name = 'logout'),
+    path('dashboard/', dashboard, name='dashboard'),
     path('', home, name = 'home')
 ]
