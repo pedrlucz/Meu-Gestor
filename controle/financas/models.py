b75cc10efb24d485bfa51cdf1789d1c742d9f157
pedrlucz <pedrlucz7@gmail.com>
2025-04-21 22:54:36 -0300

    deixando mais permissivo

--- controle/financas/models.py
+++ controle/financas/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class Categoria(models.Model):
     nome = models.CharField(max_length = 100)
+    usuario = models.ForeignKey(User, on_delete = models.CASCADE)
 
     def __str__(self):
         return self.nome
@@ -18,7 +19,7 @@ class Transacao(models.Model):
     tipo = models.CharField(max_length = 1, choices = TIPO_CHOICES)
     valor = models.DecimalField(max_digits = 10, decimal_places = 2)
     descricao = models.CharField(max_length = 200)
-    categoria = models.ForeignKey(Categoria, on_delete = models.SET_NULL, null = True)
+    categoria = models.ForeignKey(Categoria, on_delete = models.SET_NULL, null = True, blank = True)
     data = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
