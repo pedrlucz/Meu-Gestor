5b0fc6f7d8e85cdc6d4a7269f6f7433073dc4eef
vitorBud <vitor.probem@gmail.com>
2025-05-28 09:28:25 -0300

    .

--- controle/financas/views.py
+++ controle/financas/views.py
@@ -10,7 +10,7 @@ from .models import Transacao
 
 # from django.http import HttpResponseRedirect
 # from django.urls import reverse
-
+#
 def home(request):
     return render(request, 'home.html')
 
